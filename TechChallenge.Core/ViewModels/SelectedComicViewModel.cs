348e170a271436c5087f6f3cbf4a2e18e6d76fc5
Joao <joaocarvalho503@gmail.com>
2022-07-28 16:41:13 +0100

    - Added new page to when comic is selected;

---
+++ TechChallenge.Core/ViewModels/SelectedComicViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TechChallenge.Core.ViewModels
+{
+    public class SelectedComicViewModel : ObservableObject
+    {
+
+    }
+}
